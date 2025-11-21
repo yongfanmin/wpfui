af893e6f081e022d79e96214d2610e2b7c3fed8f
yangsheng <yongfanmin@gmail.com>
2025-11-21 18:22:50 +0800

    feat: 排版算法不再使用nuget引入 换成源码引入(因为需要修改算法)

---
+++ src/Wpf.Ui.Gallery/Lib/RectPackSharp/PackingException.cs
@@ -0,0 +1,18 @@
+﻿// This Source Code Form is subject to the terms of the MIT License.
+// If a copy of the MIT was not distributed with this file, You can obtain one at https://opensource.org/licenses/MIT.
+// Copyright (C) Leszek Pomianowski and WPF UI Contributors.
+// All Rights Reserved.
+
+using System;
+
+namespace RectpackSharp
+{
+    public class PackingException : Exception
+    {
+        public PackingException() : base() { }
+
+        public PackingException(string message) : base(message) { }
+
+        public PackingException(string message,  Exception innerException) : base(message, innerException) { }
+    }
+}
